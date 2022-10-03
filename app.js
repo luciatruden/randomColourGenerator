230fbdcd13389e2a6b9316b47b85dc7a5323dc18
Lucia Trujillo <ltdennis@gmail.com>
2022-10-03 10:29:28 +0200

    place random colour elements in div

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ randomButton.addEventListener('click', ()=> {
     const green = Math.floor(Math.random()*255);
     const blue = Math.floor(Math.random()*255);
 
-    // const body = document.querySelector('body');
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    const compRed = 255 - red;
+    const compGreen = 255 - green;
+    const compBlue = 255 - blue;
 
-    const heading = document.querySelector('h1');
-    heading.innerText = `rgb(${red}, ${green}, ${blue})`;
+    const redHex = red.toString(16);
+    const greenHex = green.toString(16);
+    const blueHex = blue.toString(16);
+
+    const mainColour = document.querySelector('#randomColour');
+    mainColour.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+    const rgbHeading = document.querySelector('h1');
+    rgbHeading.innerText = `rgb(${red}, ${green}, ${blue})`;
+
+    const hexHeading = document.querySelector('h2');
+    hexHeading.innerText = `#${redHex}${greenHex}${blueHex}`;
 })
